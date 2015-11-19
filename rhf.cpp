567cdd2c2674652a4d13c6e9b5d8b3dbc800c2a8
Cyril Corvazier <corvazier@mercs-eng.com>
2015-11-19 09:31:41 +0100

    [win] gettimeofday for visual c++ 2015

--- rhf.cpp
+++ rhf.cpp
@@ -83,7 +83,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#ifdef _WIN32
+#include <windows.h>
+int gettimeofday(struct timeval * tp, struct timezone * tzp)
+{
+    FILETIME    file_time;
+    SYSTEMTIME  system_time;
+    ULARGE_INTEGER ularge;
+
+    GetSystemTime(&system_time);
+    SystemTimeToFileTime(&system_time, &file_time);
+    ularge.LowPart = file_time.dwLowDateTime;
+    ularge.HighPart = file_time.dwHighDateTime;
+
+    ULONGLONG epoch = 11644473600000000ULL;
+    tp->tv_sec = (long) ((ularge.QuadPart - epoch) / 10000000L);
+    tp->tv_usec = (long) (system_time.wMilliseconds * 1000);
+
+    return 0;
+}
+#else
 #include <sys/time.h>
+#endif
 
 #include "libdenoising.h"
 #include "io_exr.h"
